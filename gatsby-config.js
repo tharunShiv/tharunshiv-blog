33ea129cad9b0745ee2b09b68033f37ea5a11037
tharunShiv <tharun.rocky14@gmail.com>
2020-02-21 12:12:44 +0530

    ga edit-1

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,18 +14,8 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-158851649-1',
-        // Puts tracking script in the head instead of the body
         head: true,
-        // IP anonymization for GDPR compliance
         anonymize: true,
-        // Disable analytics for users with `Do Not Track` enabled
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
-        // Specifies what percentage of users should be tracked
-        sampleRate: 100,
-        // Determines how often site speed tracking beacons will be sent
-        siteSpeedSampleRate: 10,
       },
     },
     'gatsby-plugin-sitemap',
